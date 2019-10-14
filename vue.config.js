00c452f63c7862d3a0e02a793bbc62b3bc5e316f
websoloho <2423458891@qq.com>
2019-10-14 23:14:52 +0800

    开启build的Gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin  = require("compression-webpack-plugin");
+
 module.exports = {
   publicPath:"/usr/themes/stime/",
   pages:{
@@ -5,6 +7,17 @@ module.exports = {
       entry:"./src/main.js",
       template:"./public/index.html",
       filename:"index.php"
+    }
+  },
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production'){
+      return {
+        plugins: [new CompressionPlugin({
+          test:/\.js$|\.html$|\.css/,
+          threshold:8000,
+          deleteOriginalAssets:false
+        })]
+      }
     }
   }
 }
